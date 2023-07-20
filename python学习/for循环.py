5ee9f1576b746bc086b6fcef934fda583f1f3427
Marco <59686577+Marco-zcl@users.noreply.github.com>
2023-07-21 01:54:22 +0800

    Update for循环.py

--- python学习/for循环.py
+++ python学习/for循环.py
@@ -13,9 +13,6 @@ for index in range(len(fruits)):
 
 print("Good bye!")
 ##4
-# !/usr/bin/python
-# -*- coding: UTF-8 -*-
-
 for num in range(10, 20):  # 迭代 10 到 20 之间的数字
     for i in range(2, num):  # 根据因子迭代
         if num % i == 0:  # 确定第一个因子
